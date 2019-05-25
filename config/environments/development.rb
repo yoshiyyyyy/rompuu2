6606db09b850444b0c4ba072f4f79c1745e56cc2
yoshimasa1125 <yoshimasahosoi1125@gmail.com>
2019-05-25 11:16:29 +0900

    追記

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,6 +45,12 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  # Add the font path
+  config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+
+  # Include font files to Assets
+  config.assetsprecompile << /\.(?:svg|eot|woff|ttf)$/
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
