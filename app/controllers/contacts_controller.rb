81e7c19af8815abfc5e3908b4baa8412333afec8
yoshimasa1125 <yoshimasahosoi1125@gmail.com>
2019-06-07 17:12:46 +0900

    メッセージ送信完了ページ

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -8,8 +8,6 @@ class ContactsController < ApplicationController
     @contact = Contact.new(contact_params)
     if @contact.save
       ContactMailer.contact_mail(@contact).deliver
-      flash[:success] = 'お問い合わせを受け付けました'
-      redirect_to root_path
     else
       render :new
     end
--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,6 +2,6 @@ class ContactMailer < ApplicationMailer
   default to: 'yoshimasahosoi1125@gmail.com' 
   def contact_mail(contact)
     @contact = contact  
-    mail(subject: 'webサイトよりメッセージが届きました') 
+    mail(subject: '龍風HPからのお問い合わせ！') 
   end
 end
