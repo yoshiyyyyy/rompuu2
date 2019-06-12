ab9f7cb2e4d1e8129375b308d789bd7a094d1d6f
yoshimasa1125 <yoshimasahosoi1125@gmail.com>
2019-06-12 15:13:54 +0900

    見出し追加

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,7 @@
 class Contact < ApplicationRecord
- 
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+  validates :message, presence: true
+  validates :email, presence: true
 end
 
